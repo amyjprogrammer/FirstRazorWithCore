bcaeaa636a342906d4eac7fe154f03f9bc7146b0
amyjprogrammer <amyjprogrammer@gmail.com>
2021-12-05 19:36:00 -0500

    Add project files.

---
+++ FirstRazorWithCore/Data/FirstRazorWithCoreContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using FirstRazorWithCore.Models;
+
+namespace FirstRazorWithCore.Data
+{
+    public class FirstRazorWithCoreContext : DbContext
+    {
+        public FirstRazorWithCoreContext (DbContextOptions<FirstRazorWithCoreContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<FirstRazorWithCore.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ FirstRazorWithCore/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using FirstRazorWithCore.Data;
+using FirstRazorWithCore.Models;
+
+namespace FirstRazorWithCore.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly FirstRazorWithCore.Data.FirstRazorWithCoreContext _context;
+
+        public IndexModel(FirstRazorWithCore.Data.FirstRazorWithCoreContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Movie = await _context.Movie.ToListAsync();
+        }
+    }
+}
